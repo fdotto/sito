5f26e73dc44c5097e49e2dfe768489b2c36f42f3
Fabio Dotto <dotto.fabio@gmail.com>
2026-04-21 20:39:16 +0200

    primo commit

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Fabio Dotto",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
